ced91f22c346e21f72e4ab2fb3e5bd30d3e0e983
friendlyanon <friendlyanon@users.noreply.github.com>
2021-09-16 16:00:27 +0200

    Refer to BUILDING.md in the in-source guard

--- cmake-init/templates/common/cmake/prelude.cmake
+++ cmake-init/templates/common/cmake/prelude.cmake
@@ -4,7 +4,7 @@ if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
   message(
       FATAL_ERROR
       "In-source builds are not supported. "
-      "Make a new directory (e.g., 'build/'), and run CMake from there. "
+      "Please read the BUILDING document before trying to build this project. "
       "You may need to delete 'CMakeCache.txt' and 'CMakeFiles/' first."
   )
 endif()
