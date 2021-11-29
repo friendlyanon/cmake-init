06cdb24967705a7959d33a20e9dee2b8b2b7c485
friendlyanon <friendlyanon@users.noreply.github.com>
2021-11-29 19:38:03 +0100

    Move C4251 suppression entirely into the header
    The macro function should be populated in the header itself, because not
    only are there more compilers on Windows than just MSVC, but this header
    gets installed, so the check needs to happen in the consuming code.
    
    The check for __ICL is taken from the Hedley library, which makes this
    suppression exclude the Intel compiler on Windows. I don't have the
    Intel compiler to check whether this is correct.

--- cmake-init/templates/common/cmake/variables.cmake
+++ cmake-init/templates/common/cmake/variables.cmake
@@ -12,10 +12,14 @@ endif()
 {if suppress}# ---- Suppress C4251 on Windows ----
 
 # Please see include/%(name)s/%(name)s.hpp for more details
-set(pragma_suppress_c4251 "#define %(uc_name)s_SUPPRESS_C4251")
-if(MSVC)
-  string(APPEND pragma_suppress_c4251 [[ _Pragma("warning(suppress:4251)")]])
-endif()
+set(pragma_suppress_c4251 "
+/* This needs to suppress only for MSVC */
+#if defined(_MSC_VER) && !defined(__ICL)
+#  define %(uc_name)s_SUPPRESS_C4251 _Pragma(\"warning(suppress:4251)\")
+#else
+#  define %(uc_name)s_SUPPRESS_C4251
+#endif
+")
 
 {end}# ---- Warning guard ----
 
