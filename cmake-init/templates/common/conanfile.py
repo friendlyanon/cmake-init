dd8de1919063d4c10ae7dff70cb6ffec0cfccf20
friendlyanon <friendlyanon@users.noreply.github.com>
2023-01-04 22:55:58 +0100

    Update Conan and vcpkg dependencies

--- cmake-init/templates/common/conanfile.py
+++ cmake-init/templates/common/conanfile.py
@@ -13,9 +13,9 @@ class Recipe(ConanFile):
 
     def requirements(self):{% if c %}{% if exe %}
         self.requires("hedley/15"){% end %}
-        self.requires("json-c/0.15"){% else %}
+        self.requires("json-c/0.16"){% else %}
         self.requires("fmt/9.1.0"){% end %}
 
     def build_requirements(self):{% if catch3 %}
-        self.test_requires("catch2/3.1.0"){% else %}
+        self.test_requires("catch2/3.2.1"){% else %}
         self.test_requires("catch2/2.13.9"){% end %}
