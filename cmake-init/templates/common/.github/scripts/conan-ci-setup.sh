55139bc41061ed19383eaf000badd4c2cf1ebf59
friendlyanon <friendlyanon@users.noreply.github.com>
2023-12-03 19:33:08 +0100

    Use Conan cache save and restore in CI

--- cmake-init/templates/common/.github/scripts/conan-ci-setup.sh
+++ cmake-init/templates/common/.github/scripts/conan-ci-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-set -e
+PS4='\033[1;34m>>>\033[0m '
+
+set -xeu
+
+pip3 install conan
 
 conan profile detect -f
 
@@ -14,3 +18,10 @@ profile="$(conan profile path default)"
 mv "$profile" "${profile}.bak"
 sed 's/^\(compiler\.cppstd=\).\{1,\}$/\1'"$std/" "${profile}.bak" > "$profile"
 rm "${profile}.bak"
+
+if [ -f conan_cache_save.tgz ]; then
+  conan cache restore conan_cache_save.tgz
+fi
+conan remove \* --lru=1M -c
+conan install . -b missing
+conan cache save '*/*:*' --file=conan_cache_save.tgz
