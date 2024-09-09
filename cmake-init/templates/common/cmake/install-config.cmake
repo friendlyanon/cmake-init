e3c89a1e0d2ff784fda3cf9c64d376bf252084f5
friendlyanon <friendlyanon@users.noreply.github.com>
2024-09-09 12:00:02 +0200

    Accomodate imports without `REQUIRED` as well

--- cmake-init/templates/common/cmake/install-config.cmake
+++ cmake-init/templates/common/cmake/install-config.cmake
@@ -1,4 +1,6 @@
 {% if pm %}include(CMakeFindDependencyMacro)
 find_dependency({% if c %}json-c{% else %}fmt{% end %})
 
-{% end %}include("${CMAKE_CURRENT_LIST_DIR}/{= name =}Targets.cmake")
+if({% if c %}json-c{% else %}fmt{% end %}_FOUND)
+  {% end %}include("${CMAKE_CURRENT_LIST_DIR}/{= name =}Targets.cmake"){% if pm %}
+endif(){% end %}
