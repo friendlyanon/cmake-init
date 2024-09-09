3a4d395b2cef3084adf0003fe4bfb15c431729b2
friendlyanon <friendlyanon@users.noreply.github.com>
2024-09-09 21:22:19 +0200

    Update dependencies

--- cmake-init/templates/common/conanfile.py
+++ cmake-init/templates/common/conanfile.py
@@ -11,8 +11,8 @@ class Recipe(ConanFile):
     def requirements(self):{% if c %}{% if exe %}
         self.requires("hedley/15"){% end %}
         self.requires("json-c/0.17"){% else %}
-        self.requires("fmt/10.2.1"){% end %}
+        self.requires("fmt/11.0.2"){% end %}
 
     def build_requirements(self):{% if catch3 %}
-        self.test_requires("catch2/3.6.0"){% else %}
+        self.test_requires("catch2/3.7.0"){% else %}
         self.test_requires("catch2/2.13.10", options={"with_main": True}){% end %}
