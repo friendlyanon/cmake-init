ec8b843e040e220c88abdce29357109121fd91b6
VeygaX <84923630+veygax@users.noreply.github.com>
2025-07-26 19:45:40 +0100

    Include C++23 standard

--- cmake-init/cmake_init.py
+++ cmake-init/cmake_init.py
@@ -50,7 +50,7 @@ class Language:
 
 c_lang = Language("C", ["e", "s", "h"], ["90", "99", "11", "17", "23"], 1)
 
-cpp_lang = Language("C++", ["e", "h", "s"], ["11", "14", "17", "20"], 2)
+cpp_lang = Language("C++", ["e", "h", "s"], ["11", "14", "17", "20", "23"], 2)
 
 
 def not_empty(value):
