b4d3c1bd14858eb3beb8fa4ecbea135506e8a517
friendlyanon <friendlyanon@users.noreply.github.com>
2025-08-31 18:30:51 +0200

    Check for required components in install interface

--- cmake-init/templates/common/cmake/install-config.cmake
+++ cmake-init/templates/common/cmake/install-config.cmake
@@ -1,6 +1,14 @@
 {% if pm %}set({= name =}_FOUND YES)
 
-include(CMakeFindDependencyMacro)
+{% end %}foreach(comp IN LISTS {= name =}_FIND_COMPONENTS)
+  if(DEFINED "{= name =}_FIND_REQUIRED_${comp}" AND "${{= name =}_FIND_REQUIRED_${comp}}")
+    set({= name =}_FOUND NO)
+    set({= name =}_NOT_FOUND_MESSAGE "This package has no components")
+    return()
+  endif()
+endforeach()
+
+{% if pm %}include(CMakeFindDependencyMacro)
 find_dependency({% if c %}json-c{% else %}fmt{% end %})
 
 if({= name =}_FOUND)
